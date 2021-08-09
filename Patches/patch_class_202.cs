d81f1758c34f7c3f5e3409166cd361cea5326f8b
Luna <izzeldeen03@gmail.com>
2021-08-09 22:55:49 +0100

    organisation // add text to main menu

---
+++ Patches/patch_class_202.cs
@@ -0,0 +1,17 @@
+﻿using MonoMod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Quintessential;
+
+class patch_class_202 {
+
+	// Render main menu
+	public static extern void orig_method_516(float param_3772, float param_3773, bool param_3774);
+	public static void method_516(float param_3772, float time, bool renderText) {
+		orig_method_516(param_3772, time, renderText);
+		if(renderText)
+			class_290.method_800($"Quintessential v{QuintessentialApi.VersionString} ({QuintessentialApi.VersionNumber})", new Vector2(49f, 70f), class_227.field_2042.field_149, class_36.field_284.WithAlpha(0.7f), 0, 1f, 0.6f, float.MaxValue, float.MaxValue, 0, new Color(), null, int.MaxValue, false, true);
+	}
+}
---
+++ Quintessential/QuintessentialApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quintessential {
+
+	class QuintessentialApi {
+
+		public readonly static string VersionString = "0.0.1";
+
+		public readonly static int VersionNumber = 1;
+	}
+}
