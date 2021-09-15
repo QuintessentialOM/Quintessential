f0af93a113d5b92e5cfe32097ef7152d5e29b7b7
Luna <izzeldeen03@gmail.com>
2021-09-15 19:20:51 +0100

    fix texture replacement

--- Patches/patch_Renderer.cs
+++ Patches/patch_Renderer.cs
@@ -6,15 +6,15 @@ class patch_Renderer {
 
 	// checks mods for textures before vanilla
 
-	public static extern bool orig_method_1343(class_256 param_5118);
+	public static extern bool orig_method_1339(class_256 param_5118);
 
-	public static bool method_1343(class_256 textureInfo) {
+	public static bool method_1339(class_256 textureInfo) {
 		if(textureInfo.field_2062.method_1085() /*Exists*/ && textureInfo.field_2062.method_1087()/*Get*/.StartsWith("Content")) {
 			Maybe<string> origPath = textureInfo.field_2062;
 			foreach(var dir in QuintessentialLoader.ModContentDirectories) {
 				try {
 					textureInfo.field_2062 = Path.Combine(dir, origPath.method_1087());
-					return orig_method_1343(textureInfo);
+					return orig_method_1339(textureInfo);
 				} catch(Exception e) {
 					if(!e.Message.StartsWith("Texture file not found!"))
 						throw;
@@ -24,6 +24,6 @@ class patch_Renderer {
 			}
 			textureInfo.field_2062 = origPath;
 		}
-		return orig_method_1343(textureInfo);
+		return orig_method_1339(textureInfo);
 	}
 }
