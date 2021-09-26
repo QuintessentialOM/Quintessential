d2eac674bb86101fccdfdc45ab636fabcd6d3c2c
Luna <izzeldeen03@gmail.com>
2021-09-26 15:10:25 +0100

    basic modlist

---
+++ Patches/patch_class_178.cs
@@ -0,0 +1,29 @@
+﻿using Quintessential;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class patch_class_178 {
+
+	public extern void orig_method_50(float param_3782);
+
+	public void method_50(float param_3782) {
+		if(GameLogic.field_2434.method_938() is ModsScreen)
+			return;
+		orig_method_50(param_3782);
+		float num = 65f;
+		Vector2 vector2_1 = new Vector2(570f, 440f);
+		Vector2 vector2_2 = (class_115.field_1433 / 2 - vector2_1 / 2).Rounded();
+		Vector2 vector2_3 = new Vector2(161f, 256f);
+		Vector2 vector2_4 = vector2_3 + new Vector2(0.0f, -num);
+		Vector2 vector2_5 = vector2_4 + new Vector2(0.0f, -num);
+		Vector2 vector2_6 = vector2_5 + new Vector2(0.0f, -num * 2);
+		if(class_140.method_314(class_134.method_253("Mods", string.Empty), vector2_2 + vector2_6).method_824(true, true)) {
+			// show mod options
+			GameLogic.field_2434.method_945(new ModsScreen(), struct_18.field_1431, struct_18.field_1431);
+		}
+	}
+}
+
---
+++ Quintessential/ModsScreen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quintessential {
+
+	class ModsScreen : IScreen {
+
+		// ???
+		public bool method_1037() {
+			return false;
+		}
+
+		// on added (true) or removed (false)
+		public void method_47(bool param_4687) {
+			
+		}
+
+		public void method_48() {
+			
+		}
+
+		// update & render
+		public void method_50(float param_4686) {
+			Vector2 size = new Vector2(570f, 440f);
+			Vector2 pos = (class_115.field_1433/*screen size*/ / 2 - size / 2).Rounded();
+			Vector2 bgPos = pos + new Vector2(78f, 88f);
+			Vector2 bgSize = size + new Vector2(-152f, -158f);
+			// background
+			class_135.method_268(class_238.field_1989.field_102.field_810, Color.White, bgPos, Bounds2.WithSize(bgPos, bgSize));
+			// frame
+			class_135.method_276(class_238.field_1989.field_102.field_817, Color.White, pos, size);
+			// label
+			class_140.method_317(class_134.method_253("Mods", string.Empty), pos + new Vector2(95f, 341f), 356, true, true);
+			// close button
+			if(class_140.method_323(pos, size, new Vector2(466f, 342f))) {
+				GameLogic.field_2434.field_2464 = false;
+				GameLogic.field_2434.method_949();
+				class_238.field_1991.field_1873.method_28(1f);
+			}
+			int y = 0;
+			foreach(var mod in QuintessentialLoader.Mods) {
+				class_135.method_290(mod.Name + " (" + mod.Version.ToString() + ")", pos - new Vector2(-100, -size.Y + 140 + y), class_238.field_1990.field_2145, class_181.field_1718, (enum_0) 0, 1f, 0.6f, float.MaxValue, float.MaxValue, 0, new Color(), (class_256)null, int.MaxValue, false, true);
+				y += 35;
+			}
+		}
+	}
+}
