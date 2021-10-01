ffbe27d70e3bd54199925e0cdecb5801914fc20d
Luna <izzeldeen03@gmail.com>
2021-10-01 20:21:22 +0100

    display a simple version marker on GIF recorder

--- Patches/MonoModRules.cs
+++ Patches/MonoModRules.cs
@@ -2,12 +2,11 @@
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using MonoMod.InlineRT;
+using MonoMod.Utils;
 using System;
+using System.Linq;
 
 namespace MonoMod {
-
-	/*[MonoModCustomMethodAttribute(nameof(MonoModRules.PatchGameLogicInit))]
-	class PatchGameLogicInit : Attribute { }*/
 
 	[MonoModCustomMethodAttribute(nameof(MonoModRules.PatchSettingsStaticInit))]
 	class PatchSettingsStaticInit : Attribute { }
@@ -17,6 +16,9 @@ namespace MonoMod {
 
 	[MonoModCustomMethodAttribute(nameof(MonoModRules.PatchScoreManagerLoad))]
 	class PatchScoreManagerLoad : Attribute { }
+
+	[MonoModCustomMethodAttribute(nameof(MonoModRules.PatchGifRecorderFrame))]
+	class PatchGifRecorderFrame : Attribute { }
 
 	static class MonoModRules {
 
@@ -76,6 +78,26 @@ namespace MonoMod {
 				}
 			} else {
 				Console.WriteLine("Failed to modify ScoreManager loading (no body)!");
+				throw new Exception();
+			}
+		}
+
+		public static void PatchGifRecorderFrame(MethodDefinition method, CustomAttribute attrib) {
+			MonoModRule.Modder.Log("Patching GIF recorder frame rendering");
+			if(method.HasBody) {
+				ILCursor cursor = new ILCursor(new ILContext(method));
+				if(cursor.TryGotoNext(MoveType.After, instr => instr.MatchCall("class_135", "method_272"))) {
+					// "class_135.method_272(class_238.field_1989.field_81.field_613.field_632, new Vector2());"
+					TypeDefinition holder = MonoModRule.Modder.FindType("class_250").Resolve();
+					MethodDefinition to = holder.Methods.First(m => m.Name.Equals("MarkOnFrame"));
+					cursor.Emit(OpCodes.Call, to);
+					//cursor.Emit(OpCodes.Call, new MethodReference("MarkOnFrame", new TypeReference("", "class_250"))/*typeof(class_250).GetMethod("MarkOnFrame")*/);
+				} else {
+					Console.WriteLine("Failed to modify GIF recorder frame rendering (no match)!");
+					throw new Exception();
+				}
+			} else {
+				Console.WriteLine("Failed to modify GIF recorder frame rendering (no body)!");
 				throw new Exception();
 			}
 		}
---
+++ Patches/patch_class_250.cs
@@ -0,0 +1,15 @@
+﻿using MonoMod;
+using Quintessential;
+
+class patch_class_250 {
+
+	[PatchGifRecorderFrame]
+	[MonoModIgnore]
+	public extern void method_50(float param_4165);
+
+	private static void MarkOnFrame() {
+		var markerPos = new Vector2(20, 647 - 60);
+		class_135.method_272(class_238.field_1989.field_81.field_613.field_632, markerPos);
+		class_135.method_290(QuintessentialLoader.VersionString, markerPos + new Vector2(30, -2), class_238.field_1990.field_2145, Color.LightGray, (enum_0)1, 1f, 0.6f, float.MaxValue, float.MaxValue, 0, new Color(), class_238.field_1989.field_71, int.MaxValue, true, true);
+	}
+}
