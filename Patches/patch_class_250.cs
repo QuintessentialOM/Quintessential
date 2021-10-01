50d80419b751f20c39f16683b11239499220a966
Luna <izzeldeen03@gmail.com>
2021-10-01 20:50:42 +0100

    slightly better marker

--- Patches/patch_class_250.cs
+++ Patches/patch_class_250.cs
@@ -8,8 +8,9 @@ class patch_class_250 {
 	public extern void method_50(float param_4165);
 
 	private static void MarkOnFrame() {
-		var markerPos = new Vector2(20, 647 - 60);
+		var markerPos = new Vector2(826 - 60 - 40, 647 - 61);
+		var verPos = new Vector2(826 - 60 - 40 - 20, 647 - 40);
 		class_135.method_272(class_238.field_1989.field_81.field_613.field_632, markerPos);
-		class_135.method_290(QuintessentialLoader.VersionString, markerPos + new Vector2(30, -2), class_238.field_1990.field_2145, Color.LightGray, (enum_0)1, 1f, 0.6f, float.MaxValue, float.MaxValue, 0, new Color(), class_238.field_1989.field_71, int.MaxValue, true, true);
+		class_135.method_290(QuintessentialLoader.VersionString, verPos, class_238.field_1990.field_2145, Color.LightGray, (enum_0)1, 1f, 0.6f, float.MaxValue, float.MaxValue, 0, new Color(), class_238.field_1989.field_71, int.MaxValue, true, true);
 	}
 }
