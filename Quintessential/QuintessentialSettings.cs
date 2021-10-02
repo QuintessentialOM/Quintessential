51c9a9c3d2a7b961bf8d0060bdc13aadc23d02f9
Luna <izzeldeen03@gmail.com>
2021-10-02 17:43:33 +0100

    remove unused test settings

--- Quintessential/QuintessentialSettings.cs
+++ Quintessential/QuintessentialSettings.cs
@@ -2,20 +2,5 @@
 
 	public class QuintessentialSettings {
 
-		[SettingsLabel("Test A")]
-		public bool TestA;
-
-		[YamlDotNet.Serialization.YamlIgnore]
-		public SettingsButton Log = () => Logger.Log("pressed button");
-
-		[SettingsLabel("Test B")]
-		public bool TestB;
-
-		[SettingsLabel("Test C")]
-		public bool TestC;
-
-		internal void Apply() {
-			
-		}
 	}
 }
