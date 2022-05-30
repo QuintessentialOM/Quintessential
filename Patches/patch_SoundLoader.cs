6437a227d891d558fe9a892be5687be4150136f8
icwass <isaac.wass@hotmail.com>
2022-05-29 23:27:44 -0500

    added QApi.fetchPath, changed some whitespace formatting

---
+++ Patches/patch_SoundLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using MonoMod;
+using Quintessential;
+
+using Song = class_186;
+
+#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+[MonoModPatch("class_235")]
+
+
+
+class patch_SoundLoader
+{
+	public extern Sound orig_method_616(string path);
+	public Sound method_616(string path)
+	{
+		return orig_method_616(QApi.fetchPath(path,".wav"));
+	}
+
+	public extern Song orig_method_617(string path);
+
+	public Song method_617(string path)
+	{
+		return orig_method_617(QApi.fetchPath(path,".ogg"));
+	}
+}
