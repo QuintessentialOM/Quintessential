9d7988bdcb1add853d37afcbc06ee2b29f6d280c
icwass <isaac.wass@hotmail.com>
2022-05-30 21:15:24 -0500

    Revert "Delete patch_SoundLoader.cs"
    This reverts commit dcec1d95fb6ad2c5dd1fa880d47ac882e29aeb48.

---
+++ Patches/patch_SoundLoader.cs
@@ -0,0 +1,28 @@
+﻿using MonoMod;
+using Quintessential;
+using Song = class_186;
+
+#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+[MonoModPatch("class_235")]
+
+
+
+class patch_SoundLoader
+{
+	public extern Sound orig_method_616(string path);
+	public Sound method_616(string path)
+	{
+		path = QApi.fetchPath(path, ".wav");
+		return orig_method_616(path.Substring(path.Length - 4));
+	}
+
+	// Adding this causes a "Common Language Runtime detected an invalid program" error for some reason??? fix later
+	//
+	//public extern Song orig_method_617(string path);
+	//
+	//public Song method_617(string path)
+	//{
+	//	//path = QApi.fetchPath(path, ".ogg");
+	//	return orig_method_617(path);
+	//}
+}
