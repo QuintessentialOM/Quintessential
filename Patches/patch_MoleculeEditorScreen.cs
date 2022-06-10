78679d0a408bcbd82f770d4cf65a31f166acca3f
Luna <izzeldeen03@gmail.com>
2022-06-10 19:40:32 +0100

    fix custom atom type alignment in molecule editor

--- Patches/patch_MoleculeEditorScreen.cs
+++ Patches/patch_MoleculeEditorScreen.cs
@@ -1,17 +1,23 @@
 ﻿
 #pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
 
+using Quintessential;
+
 class patch_MoleculeEditorScreen {
 
 	public extern void orig_method_50(float param);
 
 	public void method_50(float param) {
 		orig_method_50(param);
-		// TODO: actually put them in the right place, and only appear on modded puzzles
-		Vector2 pos = new(410, 640);
-		foreach(var type in Quintessential.QApi.ModAtomTypes) {
-			method_1130(pos, type, true);
-			pos.Y += 100;
+		// find the correct position to put the atoms
+		Vector2 uiSize = new(1516f, 922f);
+		Vector2 corner = (Input.ScreenSize() / 2 - uiSize / 2 + new Vector2(-2f, -11f)).Rounded();
+		Vector2 atomSize = new Vector2(95f, -90f);
+		Vector2 atomPos = corner + new Vector2(169f, 754f); // vanilla atoms
+		atomPos.X += atomSize.X * 3;
+		foreach(var type in QApi.ModAtomTypes) {
+			method_1130(atomPos, type, true);
+			atomPos.Y += atomSize.Y;
 		}
 	}
 
