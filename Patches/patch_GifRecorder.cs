a13fe2aac82de00e68b4176987720ee8c9b3dfdb
Luna <izzeldeen03@gmail.com>
2022-07-28 22:00:01 +0100

    cut out puzzle editor (to reimplement)

--- Patches/patch_GifRecorder.cs
+++ Patches/patch_GifRecorder.cs
@@ -1,14 +1,18 @@
-﻿using MonoMod;
+﻿#pragma warning disable IDE0051 // Remove unused private members
+#pragma warning disable IDE1006 // Naming Styles
+
+using MonoMod;
 using Quintessential;
 
 [MonoModPatch("class_250")]
-class patch_GifRecorder {
+class patch_GifRecorder{
 
 	[PatchGifRecorderFrame]
 	[MonoModIgnore]
 	public extern void method_50(float param_4165);
 
-	private static void MarkOnFrame() {
+	// name is used in MonoModRules
+	private static void MarkOnFrame(){
 		var markerPos = new Vector2(826 - 60 - 40, 647 - 61);
 		var verPos = new Vector2(826 - 60 - 40 - 20, 647 - 40);
 		class_135.method_272(class_238.field_1989.field_81.field_613.field_632, markerPos);
