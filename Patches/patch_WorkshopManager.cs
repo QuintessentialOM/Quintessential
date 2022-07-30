000ed2d1ba8f34a3f0b0cedd5f360739df9a5e3b
Luna <izzeldeen03@gmail.com>
2022-07-30 22:22:04 +0100

    fix Browse Workshop button crashing by no-opping it

--- Patches/patch_WorkshopManager.cs
+++ Patches/patch_WorkshopManager.cs
@@ -1,8 +1,17 @@
-﻿class patch_WorkshopManager {
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable SuspiciousTypeConversion.Global
 
-	public void method_2230() {
+internal class patch_WorkshopManager{
+	
+	public void method_2230(){
 		// without the object cast, it's illegal
 		((WorkshopManager)(object)this).method_2234();
 		((WorkshopManager)(object)this).method_2235();
+	}
+
+	public void method_2233(){
+		// make the Browse button a no-op rather than crashing
 	}
 }
