8516835b887f1170e0b88f6f004364106958b67c
l-Luna <izzeldeen03@gmail.com>
2023-07-02 18:51:38 +0100

    more work on screenshotting

--- Quintessential/Internal/Screenshotter.cs
+++ Quintessential/Internal/Screenshotter.cs
@@ -22,19 +22,24 @@ internal class Screenshotter{
 			};
 			var buffer = new byte[rect.x * rect.y * 4];
 
-			GCHandle bufferHandle = GCHandle.Alloc(buffer);
-			IntPtr bufferPointer = (IntPtr)bufferHandle;
-
-			IntPtr sdlRenderer = SDL.SDL_GetRenderer(GameLogic.field_2434.field_2437.field_2192);
-			SDL.SDL_RenderReadPixels(sdlRenderer, ref rect, SDL.SDL_PIXELFORMAT_RGBA8888, bufferPointer, rect.w * 4);
-
-			var surface = SDL.SDL_CreateRGBSurfaceFrom(bufferPointer, rect.w, rect.h, 8 * 4, rect.w * 4, 0xFF, 0xFF00, 0xFF0000, 0xFF000000);
-			Logger.Log("created surface!");
-			class_267.method_726(surface, Path.Combine(QuintessentialLoader.PathScreenshots, "screenshot-" + 1 + ".png"));
-			Logger.Log("saved screenshot!");
-
-			SDL.SDL_FreeSurface(surface);
-			bufferHandle.Free();
+			unsafe{
+				fixed(byte* pixels = buffer){
+					IntPtr pixPtr = new IntPtr(pixels);
+					//IntPtr sdlRenderer = SDL.SDL_GetRenderer(GameLogic.field_2434.field_2437.field_2192);
+					//SDL.SDL_RenderReadPixels(sdlRenderer, ref rect, SDL.SDL_PIXELFORMAT_ABGR8888, pixPtr, rect.w * 4);
+					IntPtr surface = SDL.SDL_CreateRGBSurfaceFrom(pixPtr, rect.w, rect.h, 32, rect.w * 4, 0xFF, 0xFF00, 0xFF0000, 0xFF000000);
+					if(surface.Equals(IntPtr.Zero))
+						throw new Exception($"Failed to create surface! \"{SDL.SDL_GetError()}\"");
+					Logger.Log("Created surface!");
+					
+					var result = class_267.method_726(surface, Path.Combine(QuintessentialLoader.PathScreenshots, "screenshot-" + 1 + ".png"));
+					if(result != 0)
+						throw new Exception($"Failed to save screenshot! \"{SDL.SDL_GetError()}\"");
+					Logger.Log("Took screenshot!");
+					
+					SDL.SDL_FreeSurface(surface);
+				}
+			}
 		}
 	}
 }
