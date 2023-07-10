dfb8185329f019270b5ba16e9b24dfddd9b258fc
l-Luna <izzeldeen03@gmail.com>
2023-07-10 11:09:56 +0100

    add helpers for finding atoms and held grippers in a `Sim`

--- Patches/patch_Sim.cs
+++ Patches/patch_Sim.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using MonoMod;
 using Quintessential;
 
@@ -19,10 +20,40 @@ class patch_Sim{
 	[MonoModPublic]
 	public List<Sim.struct_122> field_3826;
 	
+	// Hold onto held grippers
+	public List<Part> HeldGrippers;
+
+	// Helper methods to find held or unheld atoms
+	public Maybe<AtomReference> FindAtomRelative(Part part, HexIndex offset){
+		return FindAtom(part.method_1184(offset));
+	}
+	
+	public Maybe<AtomReference> FindAtom(HexIndex position){
+		var simStates = field_3821;
+		foreach(Molecule molecule in field_3823){
+			if(molecule.method_1100().TryGetValue(position, out Atom atom)){
+				bool isHeld = HeldGrippers != null && HeldGrippers.Any(part => simStates[part].field_2724 == position);
+				return new AtomReference(molecule, position, atom.field_2275, atom, isHeld);
+			}
+		}
+
+		return struct_18.field_1431;
+	}
+
 	// Run custom behaviours
 	public extern void orig_method_1832(bool first);
 	public void method_1832(bool first){
+		// fill the list of grippers
+		List<Part> allParts = field_3818.method_502().field_3919;
+		Dictionary<Part, PartSimState> simStates = field_3821;
+		HeldGrippers = new();
+		foreach(var part in allParts)
+			foreach(var gripper in part.field_2696)
+				if(simStates[gripper].field_2729.method_1085())
+					HeldGrippers.Add(gripper);
+		// run the cycle
 		orig_method_1832(first);
+		// and then process things that happen after
 		foreach(var action in QApi.ToRunAfterCycle)
 			action((Sim)(object)this, first);
 	}
