cda11aca83072ccc94ba9929432d8e155c51882e
l-Luna <izzeldeen03@gmail.com>
2023-07-10 02:18:17 +0100

    copy molecule names with molecules, fixes molecule editor wiping names

---
+++ Patches/patch_Molecule.cs
@@ -0,0 +1,11 @@
+﻿#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+public class patch_Molecule{
+
+	private extern Molecule orig_method_1104();
+	public Molecule method_1104(){
+		Molecule ret = orig_method_1104();
+		ret.field_2639 = ((Molecule)(object)this).field_2639;
+		return ret;
+	}
+}
