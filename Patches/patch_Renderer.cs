64b64569d494d2063224d7f48e16c2dceba9cddf
l-Luna <izzeldeen03@gmail.com>
2023-07-10 09:05:15 +0100

    try to provide better error messages when SDL fails to load images

--- Patches/patch_Renderer.cs
+++ Patches/patch_Renderer.cs
@@ -10,37 +10,43 @@ class patch_Renderer {
 
 	public static extern bool orig_method_1339(class_256 param_5118);
 
-	public static bool method_1339(class_256 textureInfo) {
+	public static bool method_1339(class_256 textureInfo){
 		string origPath = null;
-		if(textureInfo.field_2062.method_1085() /*Exists*/) {
+		if(textureInfo.field_2062.method_1085() /*Exists*/){
 			origPath = textureInfo.field_2062.method_1087();
 			if(textureInfo.field_2062.method_1087() /*Get*/.StartsWith("Content")) {
-				for(int i = QuintessentialLoader.ModContentDirectories.Count - 1; i >= 0; i--) {
+				for(int i = QuintessentialLoader.ModContentDirectories.Count - 1; i >= 0; i--){
 					string dir = QuintessentialLoader.ModContentDirectories[i];
-					try {
+					try{
 						textureInfo.field_2062 = Path.Combine(dir, origPath);
 						return orig_method_1339(textureInfo);
-					} catch(Exception e) {
-						if(!e.Message.StartsWith("Texture file not found!"))
-							throw;
-					} finally {
+					}catch(Exception e){
+						HandleException(e);
+					}finally {
 						textureInfo.field_2062 = origPath;
 					}
 				}
 			}
 		}
-		try {
+		try{
 			return orig_method_1339(textureInfo);
-		} catch(Exception e) {
-			if(!e.Message.StartsWith("Texture file not found!"))
-				throw;
+		}catch(Exception e) {
+			HandleException(e);
 		}
 		// none match -> use missing texture
-		try {
+		try{
 			textureInfo.field_2062 = Path.Combine(QuintessentialLoader.ModContentDirectories[0], "Content", "Quintessential", "missing");
 			return orig_method_1339(textureInfo);
-		} finally {
+		}finally{
 			textureInfo.field_2062 = origPath;
 		}
+	}
+
+	private static void HandleException(Exception e){
+		if(e.Message.StartsWith("Texture file not found!"))
+			return;
+		if(e is class_266 && e.Message.StartsWith("Failed to load PNG file:"))
+			throw new Exception($"SDL failed to load a PNG: \"{SDL2.SDL.SDL_GetError()}\"", e);
+		throw e;
 	}
 }
