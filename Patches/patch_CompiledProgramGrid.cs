68c23430fa268926cbbb91735281a2a1a92c9373
icwass <isaac.wass@hotmail.com>
2024-01-06 21:35:48 -0600

    fix DivideByZero crash when recording gifs
    the crash occurs when a solution has an instruction count of zero (like in some overlap solves)

---
+++ Patches/patch_CompiledProgramGrid.cs
@@ -0,0 +1,25 @@
+﻿using MonoMod;
+using MonoMod.Utils;
+
+using System.Collections.Generic;
+using System.Linq;
+
+#pragma warning disable CS0626 // Method, operator, or accessor is marked external and has no attributes on it
+
+public class patch_CompiledProgramGrid
+{
+	[MonoModIgnore]
+	Dictionary<Part, CompiledProgram> field_2368;
+
+	public extern int orig_method_853(int param_4510);
+
+	public int method_853(int param_4510)
+	{
+		if (this.field_2368.Count == 0) return 0;
+
+		int num = this.field_2368.Values.First().field_2367.Length;
+		if (num == 0) return 0;
+		
+		return param_4510 % num;
+	}
+}
